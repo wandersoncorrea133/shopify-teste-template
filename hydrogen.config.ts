c59b91e28802213b63366be844f2f202063aa663
wandersondacosta <wanderson_c_c@hotmail.com>
2023-01-31 15:35:34 -0300

    initiate shopify trainning

--- hydrogen.config.ts
+++ hydrogen.config.ts
@@ -6,11 +6,11 @@ export default defineConfig({
     defaultLanguageCode: 'EN',
     storeDomain:
       // @ts-ignore
-      Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'hydrogen-preview.myshopify.com',
+      Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'livreeleve1.myshopify.com',
     storefrontToken:
       // @ts-ignore
       Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN ||
-      '3b580e70970c4528da70c98e097c2fa0',
+      '8dd64c2a2de2a7484a825893e81aa932',
     privateStorefrontToken:
       // @ts-ignore
       Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN,
